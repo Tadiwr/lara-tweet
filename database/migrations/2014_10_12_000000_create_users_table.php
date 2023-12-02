7f20dfedd9a63680dea646775e5b6ceb688930e0
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-02 21:09:25 +0200

    just creates a a tweet factory nothing much

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar_url')->default('');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('follower_count')->default(0);
+            $table->integer('following_count')->default(0);
+            $table->string('username')->unique();
+
             $table->rememberToken();
             $table->timestamps();
         });
