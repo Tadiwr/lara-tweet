e41c0408d343540cbabf64464dfb3ae43b10279b
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-02 20:52:49 +0200

    adds tweet model

---
+++ app/Models/Tweet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    // Defines the relationship that a tweet is related to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
