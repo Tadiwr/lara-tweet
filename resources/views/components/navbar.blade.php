d68cfceea538c00b51c0eaca3c17485abb519e9e
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-02 21:56:34 +0200

    adds nav bar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,19 @@
+<div class=" grid grid-cols-2 px-5  w-full h-20 bg-blue-400 text-white  justify-start items-center" >
+
+
+    {{-- Logo --}}
+    <div class="h-full w-1/4 flex justify-start items-center" >
+        <a href="/" class="text-4xl font-extrabold font-mono" >LaraTweet</a>
+    </div>
+
+    {{-- Auth Action --}}
+    <div class="h-full w-full flex justify-end items-center" >
+
+        @guest
+            <a href="/login" class="text-xl " >Log In</a>
+        @else
+            <a href="/account/{{Auth::user()->username}}" class="text-xl " >Hello, {{Auth::user()->name}}</a>
+        @endguest
+    </div>
+
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-index-layout>
+    <div class="flex flex-col justify-center items-center m-10">
+        <h1>There are no tweets here</h1>
+        <a href="/tweet/make">Make a new tweet!</a>
+    </div>
+
+</x-index-layout>
