8636f56bdada6e5ca919708f1c55cf599da9f82c
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-03 19:09:15 +0200

    adds text to display when no tweets are found

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,14 @@
 <x-index-layout>
-    <div class="h-full flex flex-col m-10">
-        @foreach ($tweets as $index => $tweet)
+    <div class="h-full flex flex-col m-2">
+        @forelse ($tweets as $index => $tweet)
             <x-tweet-card :time="$times[$index]" :tweet="$tweet"></x-tweet-card>
-        @endforeach
+        @empty
+            <div class="w-full h-full flex flex-col mt-20 justify-center items-center" >
+                <h1 class="text-5xl" >There are no tweets 🙉</h1>
+                <br>
+                <a class="text-blue-300 hover:text-blue-600"  href="/tweet" >Be the first to post a tweet</a>
+            </div>
+        @endforelse
+
     </div>
 </x-index-layout>
